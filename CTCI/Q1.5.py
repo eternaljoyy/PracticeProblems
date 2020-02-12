cd1c319ccea28766d291608c082ca4d128140622
Joydeepgill <joyydeep.gill@gmail.com>
2020-02-12 11:40:21 -0500

    Still working on Q1.5

--- CTCI/Q1.5.py
+++ CTCI/Q1.5.py
@@ -1,9 +1,15 @@
 class stringCompression: 
 	
 	def compressString(n):
+		string = ''
 		arr = []
+		count = 0 
 		for item in n:
-			#Count the number of times the character appears 
-			print(n.count(item))
+			if(n.index(item) == 0):
+				count+=1
+				string = item + str(count)
+				arr.append(string)
+		return arr
+
 
 	print(compressString('aabcccccaaa'))
