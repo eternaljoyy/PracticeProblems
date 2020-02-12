dd8b3bae6ef553254468d5373302229e5915c750
Joydeepgill <joyydeep.gill@gmail.com>
2020-02-12 09:23:30 -0500

    Question 1.4 done from Ch1.

---
+++ CTCI/Q1.4.py
@@ -0,0 +1,10 @@
+
+class ReplaceWith: 
+
+	'''Whenever encountering a whitespace in 
+	n, replace whitespace with %20 instead.''' 
+	def replaceStr20(n):
+		return n.replace(" ", "%20")
+
+
+	print(replaceStr20('Mr John Smith'))
