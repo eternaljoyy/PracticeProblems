2067a493fd67538b4a20033aa4088af2c83673ce
Joydeepgill <joyydeep.gill@gmail.com>
2020-03-10 22:13:31 -0400

    removed comment (TODO)

--- Other/validPhoneNum.py
+++ Other/validPhoneNum.py
@@ -5,7 +5,6 @@ class validPhoneNum:
 	#Return true if phone number is valid, else return false. 
 	def validPhoneNum(phoneNum):
 		#regex expression for phone number 
-		#TODO: make sure of the proper order
 		pattern = '\d{3}-\d{3}-\d{4}'
 
 		#check if user input matches the pattern 
