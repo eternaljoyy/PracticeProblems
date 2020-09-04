62020a75b56f5c3e8d363c2da9c7c67a73ec0719
Joydeepgill <joyydeep.gill@gmail.com>
2020-09-04 16:22:58 -0400

    Shortend soplution to one line

--- Edabit/Is_Equal.py
+++ Edabit/Is_Equal.py
@@ -1,9 +1,6 @@
 def is_equal(lst): 
-    #Split the numbers 
-    firstNum = list(map(int, str(lst[0])))
-    secondNum = list(map(int, str(lst[1])))
-
-    return sum(firstNum) == sum(secondNum)
+    #Assumption: both of the numbers are not null 
+    return sum(map(int, str(lst[0]))) == sum(map(int, str(lst[1])))
 
 print(is_equal([105, 42]))
 print(is_equal([14, 21]))
