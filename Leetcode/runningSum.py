af4fdd3fbcb36e52bf10d89fa7b1e371c1c7861a
Joydeep Gill <joydeepgill@Joydeeps-MacBook-Air.local>
2020-11-19 15:19:51 -0500

    Solved runningSum.py using non-recursive approach

---
+++ Leetcode/runningSum.py
@@ -0,0 +1,19 @@
+def runningSum(nums):
+	#base case A) Empty list or list has 1 element 
+	if(len(nums) == 1 or nums == []):
+		return nums 
+
+	count = 1
+	for i in range(len(nums)):
+		if(i == 0):
+			count = nums[i]
+			nums[i] = count
+		else: 
+			# sum the current value of nums w/ previous value of count 
+			count = count + nums[i]
+			nums[i] = count 
+	return nums 
+
+print(runningSum([1,2,3,4]))
+print(runningSum([1,1,1,1,1]))
+print(runningSum([3,1,2,10,1]))
