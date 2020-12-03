d48a18e8673e4e8f694ae29fbd50dea8f5dc80d3
Joydeep Gill <joydeepgill@Joydeeps-MacBook-Air.local>
2020-12-03 01:05:21 -0500

    made a class and changed the position of the break statement

--- Other/MatrixOperations.py
+++ Other/MatrixOperations.py
@@ -1,28 +1,35 @@
-def max_from_ops(newMatrix, operations): 
-	#TODO: FIgure out how to loop through multiple operations 
-	rowOp = operations[0] 
-	colOp = operations[1]
+class MatrixOperations: 
+	def max_from_ops(self, newMatrix, operations): 
+		rowOp = operations[0]
+		colOp = operations[1]
+		#TODO: FIgure out how to loop through multiple operations 
+				
+		for row in range(len(newMatrix)):
+			if(row < rowOp):
+				for col in range(len(newMatrix[row])):
+					if(col < colOp):
+						newMatrix[row][col] += 1
+					else:
+						break  
+			else:
+				break
+		return newMatrix
 
-	for row in range(len(newMatrix)):
-		if(row < rowOp):
-			for col in range(len(newMatrix[row])):
-				if(col < colOp):
-					newMatrix[row][col] += 1
-				else:
-					break  
-		else:
-			break
-	return newMatrix
-			
+	#Calls the max_from_ops function (doing each operation one by one)
+	def operationsList(self, newMatrix, operations):
 
-def create_matrix(m, n):
-	newMatrix = []	
+		for item in operations:
+			self.max_from_ops(newMatrix, item)
 
-	for row in range(m):
-		rows = []
-		for col in range(n):
-			rows.append(0) 
-		newMatrix.append(rows)
-	print(max_from_ops(newMatrix, [1,1]))
+	def create_matrix(self, m, n):
+		newMatrix = []	
 
-print(create_matrix(4,4))
+		for row in range(m):
+			rows = []
+			for col in range(n):
+				rows.append(0) 
+			newMatrix.append(rows)
+		self.operationsList(newMatrix, [[1,1], [2,2]])
+
+firstMatrix = MatrixOperations()
+firstMatrix.create_matrix(4,4)
