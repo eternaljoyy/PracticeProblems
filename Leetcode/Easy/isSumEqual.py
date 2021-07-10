9e6c0829b31c37cb62d5747749f89e7a567e80d8
Joydeepgill <joyydeep.gill@gmail.com>
2021-07-10 19:11:46 -0400

    doing another string q

--- Leetcode/Easy/isSumEqual.py
+++ Leetcode/Easy/isSumEqual.py
@@ -1,33 +1,26 @@
-def mapToInt(word, wordMap): 
-    '''  
-    - Map the input word to the integer equivalent 
-    ''' 
-    
-    num = ""
-
-    for char in range(len(word)): 
-        num += str(wordMap[word[char]])
-    return int(num)
-
-
 def isSumEqual(firstWord, secondWord, targetWord): 
-    ''' 
-    -create a map in order to map the letters to their numerical counterparts 
-    -Convert first and second words into integers and add them 
-    '''   
+    '''  
 
-    wordMap = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9} 
 
-    numFirstWord = mapToInt(firstWord, wordMap) 
+    ==================================================
+    Complexity Analysis: 
+        Time complexity: O(?) 
+        Space Complexity: O(?)  
+    '''
 
-    numSecondWord = mapToInt(secondWord, wordMap) 
-
-    numTargetWord = mapToInt(targetWord, wordMap) 
-
-    return numFirstWord + numSecondWord == numTargetWord
-
+    def Helper(word):
+        res = ""
     
-             
-print(isSumEqual("acb", "cba", "cdb"))   
-print(isSumEqual("aaa", "a", "aab"))  
-print(isSumEqual("aaa", "a", "aaaa"))
+        for char in word:
+            ordVal = ord(char) - ord('a')
+            res += str(ordVal)
+        return res 
+
+    return int(Helper(firstWord)) + int(Helper(secondWord)) == int(Helper(targetWord))
+
+
+
+
+print(isSumEqual("acb","cba", "cdb"))       
+print(isSumEqual("aaa", "a", "aab"))
+print(isSumEqual( "aaa", "a", "aaaa"))
