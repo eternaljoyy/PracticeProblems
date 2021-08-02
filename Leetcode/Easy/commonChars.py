06acad847b4812fe6695b67eb9a50c22c42217af
Joydeepgill <joyydeep.gill@gmail.com>
2021-08-02 15:26:20 -0400

    doing another hashmap question

---
+++ Leetcode/Easy/commonChars.py
@@ -0,0 +1,8 @@
+def commonChars(words): 
+    '''  
+    **TODO- Come back to this q
+
+    - have an array (where the common characters will be stored)
+    - go through each character in each word and check if 
+    '''
+        
