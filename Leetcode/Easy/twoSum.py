bc98b6fe30f3af004407ec30a2016d2e9b0a61b1
Joydeep <joy@Joydeeps-Air.ht.home>
2022-09-06 19:24:36 -0400

    Created file

---
+++ Leetcode/Easy/twoSum.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+
+    '''
+    Note: x + y = Target 
+
+
+    - create a dictionary in order to store and lookup some values
+    - go through nums array 
+    
+    '''
+
+
+#Test cases 
