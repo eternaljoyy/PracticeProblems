e400a5eafb9e5ff9cbbfa8413fe5820b3e24fe3b
Joydeep Gill <joyydeep.gill@gmail.com>
2023-05-29 19:45:02 -0400

    Adds init function & createEdge func

--- Data Structures and Algorithms/depth_first_search.py
+++ Data Structures and Algorithms/depth_first_search.py
@@ -1,16 +1,31 @@
-def depth_first_search():  
+class Graph:  
+
+    def __init__(self, Vertex): 
+        self.Vertex = Vertex 
+        self.adjacency_list = [[] for i in range(V)]  
+
+    # Create edges between 2 nodes 
+    def createEdge(self, node_u, node_v): 
+    
+
+    # Runs DFS 
+    def depth_first_search(graph, visited):  
     ''' 
     Algorithm for traverseing a tree or graph. Keep visiting all nodes
     until reaching a dead end (where there are no more nodes). Then, backtrack until 
-    you reach a node which has unvisited neighbors. 
+    you reach a node which has unvisited adjacent neighbors. 
 
     ==================================================================================
 
     Time Complexity: 
-    '''
+    ''' 
 
     #Check if node is visited 
     
+    visited = [] 
+
+    unvisited_node_stack = []  
 
 
-    #traverse through the graph 
+
+    #Traverse through the graph 
