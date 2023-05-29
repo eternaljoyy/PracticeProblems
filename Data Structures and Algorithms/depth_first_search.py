8b99c8b9bcb446101b097abf89da58d880f5cdb6
Joydeep Gill <joyydeep.gill@gmail.com>
2023-05-29 19:16:35 -0400

    added comments

--- Data Structures and Algorithms/depth_first_search.py
+++ Data Structures and Algorithms/depth_first_search.py
@@ -1,1 +1,16 @@
-def depth_first_search(): 
+def depth_first_search():  
+    ''' 
+    Algorithm for traverseing a tree or graph. Keep visiting all nodes
+    until reaching a dead end (where there are no more nodes). Then, backtrack until 
+    you reach a node which has unvisited neighbors. 
+
+    ==================================================================================
+
+    Time Complexity: 
+    '''
+
+    #Check if node is visited 
+    
+
+
+    #traverse through the graph 
