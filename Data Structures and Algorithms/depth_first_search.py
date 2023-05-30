78a0f17b8d38fc3b643d0264021f07d21826f170
Joydeep Gill <joyydeep.gill@gmail.com>
2023-05-30 16:36:59 -0400

    Added code for iterative depth first search

--- Data Structures and Algorithms/depth_first_search.py
+++ Data Structures and Algorithms/depth_first_search.py
@@ -6,26 +6,37 @@ class Graph:
 
     # Create edges between 2 nodes 
     def createEdge(self, node_u, node_v): 
-    
+        self.adjacency_list[node_u].append(node_v)
 
     # Runs DFS 
-    def depth_first_search(graph, visited):  
-    ''' 
-    Algorithm for traverseing a tree or graph. Keep visiting all nodes
-    until reaching a dead end (where there are no more nodes). Then, backtrack until 
-    you reach a node which has unvisited adjacent neighbors. 
+    def depth_first_search(self, node):  
+        ''' 
+        Algorithm for traverseing a tree or graph. Keep visiting all nodes
+        until reaching a dead end (where there are no more nodes). Then, backtrack until 
+        you reach a node which has unvisited adjacent neighbors. 
 
-    ==================================================================================
+        ==================================================================================
 
-    Time Complexity: 
-    ''' 
+        ** TODO - Time Complexity: 
+        ''' 
 
-    #Check if node is visited 
-    
-    visited = [] 
+        #Check if node is visited 
+        
+        visited = [False for j in range(self.Vertex)] 
 
-    unvisited_node_stack = []  
+        stack = [node] 
 
+        while len(stack) > 0:
 
+            node = stack.pop() 
 
-    #Traverse through the graph 
+            if not visited[node]:
+                print(node, end= ' ')
+                visited[node] = True 
+
+                #go through the neighbors of the current vertex
+                for item in self.adjacency_list:
+                    if not visited[item]:
+                        stack.append(item)
+
+ 
