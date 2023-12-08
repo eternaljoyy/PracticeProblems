0790a09cbf2260a356abc249b7228225cd564845
Joydeep Gill <joyydeep.gill@gmail.com>
2023-12-08 00:53:12 -0500

    cleaned up solution for rotate string

--- Leetcode/Easy/rotateString.py
+++ Leetcode/Easy/rotateString.py
@@ -3,18 +3,31 @@ def rotateString(s, goal):
         Keep shifting the string one by one to check if that new string is equal to 
         the target string goal. Stop shifting and return True when s == goal, otherwise 
         keep shifting to the right by 1; when youve shifted such that it returns back to 
-        original string then return False.   
+        original string then return False.    
+
+    ======================================================================================= 
+
+    Runtime Complexity: O(N) (loop) * O(?) (w/ slicing)
+
+    Spacetime Complexity: O(?) -> No additional data structure was made  
+    
+
+    -> TODO: Figure out time and space complexity of slicing a list 
     '''   
+    
+    numChars = len(s)
 
-    shuffleStr = s[-1:] + s[:-1] 
+    while numChars != 0: 
 
-    if shuffleStr == goal:
-        return True 
-    elif shuffleStr == s: 
-        return False 
-    else: 
-        return rotateString(shuffleStr, goal)
+        s = s[-1:] + s[:-1] 
+        
+        if s == goal:
+            return True 
+
+        numChars -= 1
+    return False 
 
 
-# print(rotateString('abcde', 'cdeab'))
+#Testcases 
+print(rotateString('abcde', 'cdeab'))
 print(rotateString('abcde', 'abced'))
