9547d4f79b206cdfafd4746e906f394079cc55d7
Joydeep Gill <joyydeep.gill@gmail.com>
2023-12-08 00:20:21 -0500

    brute force recursive soln to rotate string (Leetcode #796)

--- Leetcode/Easy/rotateString.py
+++ Leetcode/Easy/rotateString.py
@@ -1,12 +1,20 @@
 def rotateString(s, goal): 
     ''' 
         Keep shifting the string one by one to check if that new string is equal to 
-        the target string goal. Stop shifting and return true when s == goal, otherwise 
+        the target string goal. Stop shifting and return True when s == goal, otherwise 
         keep shifting to the right by 1; when youve shifted such that it returns back to 
         original string then return False.   
-    '''  
+    '''   
 
-    if s == goal:
+    shuffleStr = s[-1:] + s[:-1] 
+
+    if shuffleStr == goal:
+        return True 
+    elif shuffleStr == s: 
         return False 
     else: 
-        
+        return rotateString(shuffleStr, goal)
+
+
+# print(rotateString('abcde', 'cdeab'))
+print(rotateString('abcde', 'abced'))
