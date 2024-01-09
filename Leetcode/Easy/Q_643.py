ff263efdeafcf3024596b42f2f150a83c965ebfd
Joydeep Gill <joyydeep.gill@gmail.com>
2024-01-08 20:48:54 -0500

    Starting findMaxaverage

---
+++ Leetcode/Easy/Q_643.py
@@ -0,0 +1,21 @@
+def findMaxAverage(nums, k): 
+	''' 
+	Approach: 
+
+	- new array holding the averages of 4 numbers and 
+	will return the maximum at the last step 
+	- go through nums array with k numbers, find the average 
+	value and add it to new array 
+	- keep going through the array until you reached the end 
+	- return the maximum average value 
+
+	=========================================================
+	Complexities: 
+	- Time Complexity: 
+	- Space Complexity:  
+
+	''' 
+
+	maxAvg = [] 
+
+	
