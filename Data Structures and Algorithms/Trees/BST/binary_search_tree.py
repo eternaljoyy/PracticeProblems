6369506d8c65806441654abdc7402ce22da48383
Joydeep Gill <joyydeep.gill@gmail.com>
2024-01-26 23:36:16 -0500

    Adding BST file

---
+++ Data Structures and Algorithms/Trees/BST/binary_search_tree.py
@@ -0,0 +1,14 @@
+class BinarySearchTree: 
+
+    def __init__(self, value):
+        this.next = next 
+        this.value = value 
+
+    def insert(self, value): 
+
+    
+    def remove(self, value): 
+
+    
+
+    def search(self, value): 
