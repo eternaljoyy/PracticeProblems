90e425872b85c6d06b96e840c363e135b3b329d6
Joydeep Gill <joyydeep.gill@gmail.com>
2025-05-31 23:36:36 -0400

    added q929

---
+++ Leetcode/Q_929_NumUniqueEmails.py
@@ -0,0 +1,57 @@
+class Solution:   
+    def numUniqueEmails(self, emails):
+        """
+            :type emails: List[str]
+            :rtype: int 
+
+        ----------------------------------
+        Approach A) 
+        Go through list of emails 
+        - extract the email according to the conditions given 
+        - add email to the set (if its not already there)
+        - return length of set  
+        
+
+        ** TODO
+        """
+
+        unique_emails = set()
+
+
+        for word in range(len(emails)):
+
+            split_email = self.splitEmail(emails[word])
+            print(split_email)
+
+            if split_email not in unique_emails:
+                unique_emails.add(split_email)
+                # print(unique_emails)
+
+        return len(unique_emails)
+
+
+
+    def splitEmail(self, split_email):
+
+        # split_email = ''.join(domain.split('@'))
+        new_email = ''
+
+        for index in range(len(split_email)):
+            if split_email[index].isalnum() or split_email[index] == '.':
+                new_email += split_email[index]
+            else:
+                new_email += split_email[split_email.index('@'):]
+                break
+        return new_email
+
+
+
+#Tests 
+# email = Solution()
+# print(email.numUniqueEmails(["m.y+name@email.com"]))
+
+
+second_emails = Solution()
+print(second_emails.numUniqueEmails(["test.email+alex@leetcode.com"]))
+
+# ["test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
